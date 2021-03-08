4f5bcf989408eabf6a51a44a411a5646048668a3
kkan0615 <kkan0615@gmail.com>
2021-03-08 21:56:23 +0900

    :zap: Feat: add naviagtor file

--- src/layouts/general/index.vue
+++ src/layouts/general/index.vue
@@ -1,13 +1,26 @@
 <template>
-  {{ msg }}
-  <router-view />
+  <div
+    class="flex h-full"
+  >
+    <navigator />
+    <div class="h-full p-4">
+      <main class="space-y-4">
+        {{ msg }}
+        <router-view />
+      </main>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue'
+import Navigator from '@/components/commons/layouts/Navigator/index.vue'
 
 export default defineComponent({
   name: 'GeneralLayout',
+  components: {
+    Navigator
+  },
   setup () {
     const msg = ref('GeneralLayout File')
     return {
