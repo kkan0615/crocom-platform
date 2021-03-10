565ea6c8a97c02a2dfbfdfec280e9d38843fb4ba
kkan0615 <kkan0615@gmail.com>
2021-03-11 00:42:43 +0900

    :zap: Feat: add list component

---
+++ src/components/commons/List/components/content.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full items-center flex">
+    <div class="mx-2 -mt-1">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TContentList',
+  setup () {
+    const msg = ref('Home File')
+
+    return {
+      msg,
+    }
+  }
+})
+</script>
