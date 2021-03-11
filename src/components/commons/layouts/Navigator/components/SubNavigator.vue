e3cd39c26f24ba83eb34b2117330da93348ae54f
kkan0615 <kkan0615@gmail.com>
2021-03-12 00:36:45 +0900

    :rocket: version 0.3

---
+++ src/components/commons/layouts/Navigator/components/SubNavigator.vue
@@ -0,0 +1,26 @@
+<template>
+  <aside class="flex-shrink-0 hidden w-20 bg-white border-r dark:border-blue-800 dark:bg-darker md:block">
+    <div class="flex flex-col h-screen">
+      <nav
+        class="flex-1 px-2 py-4 space-y-2 overflow-y-hidden hover:overflow-y-auto h-full text-center"
+      >
+        <slot />
+      </nav>
+    </div>
+  </aside>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TSubNavigator',
+  setup () {
+    const msg = ref('Home File')
+
+    return {
+      msg,
+    }
+  }
+})
+</script>
