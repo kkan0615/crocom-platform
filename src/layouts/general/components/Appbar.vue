a1e4fa98ce4f7d5aacbf38827f0c2a97fc2f2b14
kkan0615 <kkan0615@gmail.com>
2021-03-17 21:29:34 +0900

    :zap: Feat: add appbar

---
+++ src/layouts/general/components/Appbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <t-app-bar />
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import TAppBar from '@/components/commons/layouts/Appbar/index.vue'
+
+export default defineComponent({
+  name: 'AppBar',
+  components: {
+    TAppBar
+  },
+  setup () {
+    const msg = ref('Home File')
+
+    return {
+      msg,
+    }
+  }
+})
+</script>
