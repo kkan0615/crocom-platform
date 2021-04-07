966819bc00c002a7092e464fff78fca11f1e852a
kkan0615 <kkan0615@gmail.com>
2021-04-08 00:05:33 +0900

    :zap: Feat: add dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import { KeyOfLocalstorage } from '@/types/system/localstorage'
+import { getCurrentTheme, ThemeMode } from '@/types/theme'
+
+export default defineComponent({
+  name: 'App',
+  setup () {
+    onMounted(() => {
+      /* Set the theme to localstorage */
+      localStorage.setItem(KeyOfLocalstorage.THEME, getCurrentTheme())
+
+      if (getCurrentTheme() === ThemeMode.DARK_MODE) {
+        if (document.documentElement.classList[ThemeMode.LIGHT_MODE])
+          document.documentElement.classList.remove(ThemeMode.LIGHT_MODE)
+        document.documentElement.classList.add(ThemeMode.DARK_MODE)
+      } else {
+        if (document.documentElement.classList[ThemeMode.DARK_MODE])
+          document.documentElement.classList.remove(ThemeMode.DARK_MODE)
+        document.documentElement.classList.add(ThemeMode.LIGHT_MODE)
+      }
+    })
+  }
+})
+</script>
